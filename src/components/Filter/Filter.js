0c262dfeef9b06f1fd97bb9d40f404d2154b8711
devisd <89788799+devisd@users.noreply.github.com>
2022-07-16 23:04:16 +0700

    add start files

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Filter = ({ filter, onChange }) => {
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input type="text" name="filter" value={filter} onInput={onChange} />
+      </label>
+    </div>
+  );
+};
+
+export default Filter;
+
+Filter.propTypes = {
+  filter: propTypes.string,
+  onChange: propTypes.func,
+};
